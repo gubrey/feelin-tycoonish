01b627b022356fe7ee5686483dbcdf9c9c6bfc0a
COGSNGEARSNBOLTS <daveiousmilleruous@gmail.com>
2025-07-16 13:57:08 -0400

    i think i am = done for right bnow
    oooooooooooo

--- map/spear.gd
+++ map/spear.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	
-	rotation = 90 + sin((Time.get_ticks_msec() / 1000.) * 1) *100
+	rotation = 90 + sin((Time.get_ticks_msec() / 10000.) * 1) *100
