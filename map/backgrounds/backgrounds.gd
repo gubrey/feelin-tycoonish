db7ba7dc8d07c013f15177da3af32e4199356fb0
kautaruma <75647306+KAUTARUMA@users.noreply.github.com>
2025-07-17 12:43:11 -0400

    add background fading

---
+++ map/backgrounds/backgrounds.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const FADE_LENGTH = 1.0
+
+var fade_tween: Tween
+
+func _ready() -> void:
+	Game.bg_changed.connect(_fade_all_bgs)
+
+func _fade_all_bgs(bg_name: String):
+	if fade_tween != null:
+		fade_tween.kill()
+	
+	fade_tween = create_tween()
+	
+	for child: Node2D in get_children():
+		var target = 0.0 if child.name != bg_name else 1.0
+		
+		fade_tween.parallel().tween_property(child, "modulate", Color(
+			1.0,
+			1.0,
+			1.0,
+			target
+		), FADE_LENGTH)
